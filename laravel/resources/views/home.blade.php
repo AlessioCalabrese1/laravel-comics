c5a5b231f3fb788afbb4d1f89440a7e527c4ac5f
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-09-07 17:41:00 +0200

    Main implementation

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.main')
 
+@php
+$mainOptions = [
+[
+'link' => "DIGITAL COMICS",
+'image' => "buy-comics-digital-comics.png"
+],
+[
+'link' => "DC MERCHENDISE",
+'image' => "buy-comics-merchandise.png"
+],
+[
+'link' => "SUBSCRIPTION",
+'image' => "buy-comics-subscriptions.png"
+],
+[
+'link' => "COMIC SHOP LOCATOR",
+'image' => "buy-comics-shop-locator.png"
+],
+[
+'link' => "DC POWER VISA",
+'image' => "buy-dc-power-visa.svg"
+],
+]
+@endphp
+
 @section('showcase')
 <main>
     <div class="jumbotron-container">
@@ -13,15 +38,15 @@
         <div class="comics-container">
             <div class="d-flex flex-wrap pt-4">
                 @foreach ($comics as $comic)
-                    <div class="comic">
-                        <div class="thumb-container">
-                            <img src="{{$comic->image}}" alt="{{$comic->title}}">
-                        </div>
-                        
-                        <p class="comic-name">
-                            {{$comic->title}}
-                        </p>
+                <div class="comic">
+                    <div class="thumb-container">
+                        <img src="{{$comic->image}}" alt="{{$comic->title}}">
                     </div>
+
+                    <p class="comic-name">
+                        {{$comic->title}}
+                    </p>
+                </div>
                 @endforeach
             </div>
 
@@ -29,18 +54,20 @@
         </div>
     </div>
 
-    {{-- <div class="main-options-container">
+    <div class="main-options-container">
         <ul>
-            <li v-for="(option, index) in mainOptions" :key="index">
-                <div class="option-image-container">
-                    <img class="option-image" :src="require(`../assets/img/${option.image}`)" :alt="option.link">
-                </div>
-                
-                <p class="option-text ps-3">
-                    {{ option.link }}
-                </p>
-            </li>
+            @foreach ($mainOptions as $item)
+                <li>
+                    <div class="option-image-container">
+                        <img class="option-image" src="{{asset('images/' . $item['image'])}}" alt="{{$item['link']}}">
+                    </div>
+
+                    <p class="option-text ps-3">
+                        {{ $item['link'] }}
+                    </p>
+                </li>
+            @endforeach
         </ul>
-    </div> --}}
+    </div>
 </main>
 @endsection
